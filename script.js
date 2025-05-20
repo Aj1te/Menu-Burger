bb5680aaff391478c4d05d9ba5a3358f632a4f9d
Aj1te <pons_julien@outlook.fr>
2025-05-20 22:20:17 +0200

    Push Final menu burger

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 
+// burger.addEventListener('click', () => {
+//   menu.classList.toggle('open');
+// });
+
 burger.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
+  if (menu.classList.contains('open')) {
+    menu.classList.remove('open');
+  } else {
+    menu.classList.add('open');
+  }
+})
